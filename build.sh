33b9438ee0c083e002e15d5ad0f7e8e334e5d0e6
coolmag <54004066+coolmag@users.noreply.github.com>
2025-08-12 01:39:11 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-   1     #!/bin/bash
-   2     apt-get update -y
-   3     apt-get install -y ffmpeg
-   4     pip install -r requirements.txt
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y ffmpeg
+    pip install -r requirements.txt
