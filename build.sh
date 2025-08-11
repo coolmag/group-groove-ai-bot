264a77aa374d9eaa1b26d33cac492e82c2173560
coolmag <54004066+coolmag@users.noreply.github.com>
2025-08-11 22:22:56 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+       #!/bin/bash
+   
+       # Install FFmpeg
+       apt-get update -y
+       apt-get install -y ffmpeg
+   
+        # Install Python dependencies
+        pip install -r requirements.txt
