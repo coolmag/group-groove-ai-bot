108ca56e91c4b61e8e4df0699abfa136bf5bc64d
coolmag <54004066+coolmag@users.noreply.github.com>
2025-08-22 17:55:06 +0300

    Add files via upload

--- locks.py
+++ locks.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
-from asyncio import Lock
+import asyncio
 
-state_lock = Lock()
-status_lock = Lock()
-refill_lock = Lock()
+# Лок для защиты состояния бота (BotState)
+state_lock = asyncio.Lock()
+
+# Лок для управления радио-задачами, чтобы избежать одновременного запуска
+radio_lock = asyncio.Lock()
+
+# Лок для управления доступом к yt-dlp, чтобы избежать параллельных загрузок
+download_lock = asyncio.Lock()
